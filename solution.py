4c3840ad1c8064d1767142d80489fb758d9dde1d
Ekaterina Eremina <31165386+Yaroina@users.noreply.github.com>
2023-04-08 23:28:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 395384260 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    alpha = 0.03  # уровень значимости
+    statistic, pvalue = ks_2samp(x, y)
+    return pvalue < alpha
