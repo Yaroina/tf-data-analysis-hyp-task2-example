4f3a64a1ddaf15509f045ace6ca81b41306258d2
Ekaterina Eremina <31165386+Yaroina@users.noreply.github.com>
2023-04-09 09:57:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 395384260 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    
-    alpha = 0.03  # уровень значимости
-    statistic, pvalue = ks_2samp(x, y)
-    return pvalue < alpha
+    alpha = 0.03
+    from scipy.stats import ks_2samp 
+    p = ks_2samp(x, y)[1]
+    return p < alpha
